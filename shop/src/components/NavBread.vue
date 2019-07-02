0471b742c4f9aa6c6abcac830327be27bc5205e9
YJM890409 <421375036@qq.com>
2019-07-02 21:37:45 +0800

    shop

---
+++ shop/src/components/NavBread.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="GoodsList">
+    <div class="nav-breadcrumb-wrap">
+      <div class="container">
+        <nav class="nav-breadcrumb">
+          <a href="/">首页</a>
+          <span>商品列表</span>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
